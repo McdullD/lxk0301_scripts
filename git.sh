ce47c95d6071ff287df47c2ba4648896cd580cc1
xingxiaodong <xingxiaodong@pzoom.com>
2020-11-12 15:26:40 +0800

    add cron

---
+++ git.sh
@@ -0,0 +1,27 @@
+#git clone https://github.com/…/PrettyGirls.git
+
+#拉取远程代码
+#git pull dev
+
+#设定远程地址
+#git remote add dev https://github.com/lxk0301/jd_scripts.git
+
+
+
+#git push -u -f dev master 
+#提交到远程仓库
+
+#git remote add origin https://github.com/lxk0301/jd_scripts.git
+
+
+#拉取git上文件 获取更新
+
+
+#推送到git2上
+
+
+
+git pull lxk master
+ 
+
+git push -u -f dev master
